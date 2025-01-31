5f412feaee7aec47b4557302c4fa8f7acbe0f5f3
Garron Seddon <garronseddon@icloud.com>
2025-01-31 09:24:29 -0500

    Task 1 - Managing the Spice Market

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1: Managing the Spice Market
+let spicePrices = [100, 120, 140, 160, 180];
+spicePrices.push(200); // New shipment of spice
+spicePrices.shift(); // Remove the first price due to sabotage
+console.log("Updated Spice Prices:", spicePrices);
+// Commit message: "Task 1 - Managing the Spice Market"
